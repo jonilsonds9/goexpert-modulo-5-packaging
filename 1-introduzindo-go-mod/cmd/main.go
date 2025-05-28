57e5782efd83f73392a8200aa376438ff2169a85
Jonilson Sousa <jonilsonds9@gmail.com>
2025-05-28 10:34:55 -0300

    Acessando pacotes criados

--- 1-introduzindo-go-mod/cmd/main.go
+++ 1-introduzindo-go-mod/cmd/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/jonilsonds9/goexpert-modulo-5-packaging/1-introduzindo-go-mod/math"
+)
 
-func main()  {
-	fmt.Println("Hello, World!")
+func main() {
+	m := math.Math{A: 1, B: 2}
+	fmt.Println(m.Add())
+	// fmt.Println("Hello, World!")
 }
